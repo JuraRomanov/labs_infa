79d2a39f2c6a34a1dc17ba2266bd3a9f4ee51197
Jura Romanov <juro4ik@gmail.com>
2023-09-17 12:37:58 +0300

    dodelal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# библиотеки
 import random
 import math
+import openpyxl
+from openpyxl.chart import BarChart, Reference
+
+# классы
 
 
 class DescrRnd:
@@ -86,6 +91,7 @@ class ConrinRnd:
     f = []
     data = []
 
+    # конструктор
     def __init__(self, _xMin=-8, _xMax=1, _numSamples=10):
         self.xMin, self.xMax = _xMin, _xMax
         self.numSamples = _numSamples
@@ -107,7 +113,7 @@ class ConrinRnd:
         return buf
 
     def CountFreqs(self):
-
+        data = []
         # Фильтрация значений, чтобы они попадали в интервал (xmin, xmax)
         filtered_data = [x for x in self.data if self.xMin <= x <= self.xMax]
         print("Набор значений НСВ с распределением Коши:")
@@ -126,7 +132,48 @@ class ConrinRnd:
         for i in range(num_bins):
             bin_start = self.xMin + i * bin_width
             bin_end = bin_start + bin_width
-            print(f'Интервал ({bin_start:.2f}, {bin_end:.2f}): {hist[i]}')
+            print(f'Интервал №{i} ({bin_start:.2f}, {bin_end:.2f}): {hist[i]}')
+            data.append([i, hist[i]])
+
+        return data
+# функции
+
+
+def DrawGistogram(_data):
+    # Создаем новую книгу Excel
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+
+    data = [
+        ['Категория', 'Значение'],
+    ]
+    data += _data
+
+    for row in data:
+        sheet.append(row)
+
+    # Создаем объект гистограммы
+    chart = BarChart()
+    chart.type = "col"
+    chart.style = 13
+    chart.title = "гистограмма распределения значений"
+    chart.x_axis.title = "Категории"
+    chart.y_axis.title = "Значения"
+
+    # Указываем диапазон данных для гистограммы
+
+    data_range = Reference(sheet, min_col=2, min_row=1,
+                           max_col=2, max_row=len(data))
+    labels = Reference(sheet, min_col=1, min_row=2, max_row=len(data))
+
+    chart.add_data(data_range, titles_from_data=True)
+    chart.set_categories(labels)
+
+    # Вставляем гистограмму в лист Excel
+    sheet.add_chart(chart, "E5")
+
+    # сохранияем книгу
+    workbook.save("гистограмма.xlsx")
 
 
 if __name__ == '__main__':
@@ -143,4 +190,6 @@ if __name__ == '__main__':
     buf = ""
 
     print(cRnd.GenKoshi(a, numSamples))
-    cRnd.CountFreqs()
+    data = cRnd.CountFreqs()
+
+    DrawGistogram(data)
